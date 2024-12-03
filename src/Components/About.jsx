c842e09c342d0789e1b3a982032fef2acb627803
Dante Schrantz <danteschrantz@gmail.com>
2024-12-03 08:10:19 +0100

    First Commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Business Administration with Finance and Acounting minor studying at the Universidad de Navarra in Pamplona, Spain. I am passionate about solving problems in new creative ways to drive innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Microsoft Excel",
+  "Microsoft PowerBI",
+  "Spanish",
+  "Legal research",
+  "Document Management",
+  "R Programming",
 ];
 
 /**
